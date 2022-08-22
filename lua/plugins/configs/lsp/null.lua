86ff0a8ace1a81a4e537c3f2a0249aec2a20f5d1
njeromin <njeromin11@gmail.com>
2022-08-22 18:25:44 +1000

    feat(null-ls): added golang formatters/linters

--- lua/plugins/configs/lsp/null.lua
+++ lua/plugins/configs/lsp/null.lua
@@ -5,9 +5,12 @@ local b = null.builtins
 null.setup({
   sources = {
     b.formatting.stylua,
+    b.diagnostics.selene,
+
     b.formatting.prettierd,
     b.formatting.rome,
 
-    b.diagnostics.selene,
+    b.formatting.gofmt,
+    b.formatting.goimports,
   }
 })
