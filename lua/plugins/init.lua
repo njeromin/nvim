9eeacb583ad472d439a08f70a70907fc0a91fb11
njeromin <njeromin11@gmail.com>
2022-08-22 10:59:45 +1000

    fix(completion): stop lazy loading completion engine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,24 +77,14 @@ M.nvim = {
   },
 
   -- completion
-  ["hrsh7th/cmp-nvim-lsp"] = {
-    event = "InsertEnter",
-  },
-  ["hrsh7th/cmp-buffer"] = {
-    event = "InsertEnter",
-  },
-  ["hrsh7th/cmp-path"] = {
-    event = "InsertEnter",
-  },
-  ["hrsh7th/cmp-cmdline"] = {
-    event = "InsertEnter",
-  },
+  ["hrsh7th/cmp-nvim-lsp"] = {},
+  ["hrsh7th/cmp-buffer"] = {},
+  ["hrsh7th/cmp-path"] = {},
+  ["hrsh7th/cmp-cmdline"] = {},
   ["rafamadriz/friendly-snippets"] = {
     module = "cmp_nvim_lsp",
-    event = "InsertEnter",
   },
   ["hrsh7th/nvim-cmp"] = {
-    after = { "cmp-nvim-lsp", "cmp-buffer", "cmp-path", "cmp-cmdline" },
     config = requirePluginConfig("lsp.completion"),
   },
   ["L3MON4D3/LuaSnip"] = {
